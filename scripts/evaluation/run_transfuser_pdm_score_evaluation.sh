1deb60916ef425f8debcbefc4c3906450b7d5321
Qiaoceng <xinqiaoceng9@gmail.com>
2025-09-06 13:52:58 +0800

    visualization with PDM(stage1)

--- scripts/evaluation/run_transfuser_pdm_score_evaluation.sh
+++ scripts/evaluation/run_transfuser_pdm_score_evaluation.sh
@@ -1,15 +1,18 @@
-TRAIN_TEST_SPLIT=navhard_two_stage
-CHECKPOINT=/mnt/hdd5/Qiaoceng/navsim_workspace/ckpt/08291123_epoch99.ckpt
+TRAIN_TEST_SPLIT=mini
+CHECKPOINT=/mnt/hdd5/Qiaoceng/navsim_workspace/model/LatentTransfuser_mini/0830.ckpt
 CACHE_PATH=/mnt/hdd5/Qiaoceng/navsim_workspace/exp/metric_cache
 SYNTHETIC_SENSOR_PATH=$OPENSCENE_DATA_ROOT/navhard_two_stage/sensor_blobs
 SYNTHETIC_SCENES_PATH=$OPENSCENE_DATA_ROOT/navhard_two_stage/synthetic_scene_pickles
 
+export HYDRA_FULL_ERROR=1
+
 python $NAVSIM_DEVKIT_ROOT/navsim/planning/script/run_pdm_score.py \
 train_test_split=$TRAIN_TEST_SPLIT \
 agent=transfuser_agent \
-worker=single_machine_thread_pool \
 agent.checkpoint_path=$CHECKPOINT \
 experiment_name=transfuser_agent \
 metric_cache_path=$CACHE_PATH \
 synthetic_sensor_path=$SYNTHETIC_SENSOR_PATH \
 synthetic_scenes_path=$SYNTHETIC_SCENES_PATH \
+worker=single_machine_thread_pool \
+worker.max_workers=4 \
--- scripts/training/run_latent_transfuser_training.sh
+++ scripts/training/run_latent_transfuser_training.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 訓練 Latent TransFuser 模型的腳本
-TRAIN_TEST_SPLIT=mini
+TRAIN_TEST_SPLIT=navtrain
 
 python $NAVSIM_DEVKIT_ROOT/navsim/planning/script/run_training.py \
 agent=transfuser_agent \
