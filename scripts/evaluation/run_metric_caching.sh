ba39fed7b29a73788f47f78b6abd12ff31b50867
Qiaoceng <xinqiaoceng9@gmail.com>
2025-09-07 12:35:32 +0800

    visulization

--- scripts/evaluation/run_metric_caching.sh
+++ scripts/evaluation/run_metric_caching.sh
@@ -1,5 +1,5 @@
-TRAIN_TEST_SPLIT=mini
-CACHE_PATH=$NAVSIM_EXP_ROOT/metric_cache
+TRAIN_TEST_SPLIT=test
+CACHE_PATH=$NAVSIM_EXP_ROOT/metric_cache_test
 
 python $NAVSIM_DEVKIT_ROOT/navsim/planning/script/run_metric_caching.py \
 train_test_split=$TRAIN_TEST_SPLIT \
